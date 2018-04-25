0edda17881175f745ab781b0d4f9d21382f2b286
練子瑜 <lientzubob@gmail.com>
2018-04-25 14:51:27 +0800

    commit2

---
+++ testProject3/src/main/java/testProject3/test3.java
@@ -0,0 +1,9 @@
+package testProject3;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		System.out.println("aaa3");
+	}
+
+}
---
+++ testProject3/src/main/java/testProject3/test4.java
@@ -0,0 +1,9 @@
+package testProject3;
+
+public class test4 {
+
+	public static void main(String[] args) {
+		System.out.println("aaa4");
+	}
+
+}
