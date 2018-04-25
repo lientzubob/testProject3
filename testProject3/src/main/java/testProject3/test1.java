a9789ff0d7b08774f886104c224e918f656e112d
kings7788 <34169622+kings7788@users.noreply.github.com>
2018-04-25 16:01:29 +0800

    fuckyouclass

--- testProject3/src/main/java/testProject3/test1.java
+++ testProject3/src/main/java/testProject3/test1.java
@@ -3,18 +3,7 @@ package testProject3;
 public class test1 {
 
 	public static void main(String[] args) {
-		System.out.println("aaa");
-		System.out.println("ssss");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
-		System.out.println("asdfasdfasdf");
+	System.out.println("asdfasdfasdf");
 	}
 
 }
